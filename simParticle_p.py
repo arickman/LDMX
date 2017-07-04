96e7732b458f6ffa23da225c73c57d7554e70ea7
arickman <arickman@stanford.edu>
2017-07-03 23:39:50 -0700

    debug

--- simParticle_p.py
+++ simParticle_p.py
@@ -20,47 +20,47 @@ from matplotlib.colors import LogNorm
 from numpy import linalg as la
 from scipy.stats import norm
 
-parser = argparse.ArgumentParser(description='')
-parser.add_argument('-i', action='store', dest='rfile_path', 
-                    help='ROOT file to processed.')
-args = parser.parse_args()
+# parser = argparse.ArgumentParser(description='')
+# parser.add_argument('-i', action='store', dest='rfile_path', 
+#                     help='ROOT file to processed.')
+# args = parser.parse_args()
 
-if not args.rfile_path:
-    parser.error('A ROOT file needs to be specified')
+# if not args.rfile_path:
+#     parser.error('A ROOT file needs to be specified')
 
-ldmx_lib_path = '%s/lib/libEvent.so' % os.environ['LDMX_SW_DIR']
-r.gSystem.Load(ldmx_lib_path)
+# ldmx_lib_path = '%s/lib/libEvent.so' % os.environ['LDMX_SW_DIR']
+# r.gSystem.Load(ldmx_lib_path)
 
-rfile = r.TFile(args.rfile_path)
-tree = rfile.Get("LDMX_Events")
+# rfile = r.TFile(args.rfile_path)
+# tree = rfile.Get("LDMX_Events")
 
-sParticles = r.TClonesArray('ldmx::SimParticle')
-tree.SetBranchAddress("SimParticles_sim", r.AddressOf(sParticles))
+# sParticles = r.TClonesArray('ldmx::SimParticle')
+# tree.SetBranchAddress("SimParticles_sim", r.AddressOf(sParticles))
 
-momentum = []
-for entry in xrange(0, tree.GetEntries()):
-    tree.GetEntry(entry)
-    pSum = 0
-    for sParticle in sParticles :
-        pSum += np.linalg.norm(sParticle.getMomentum())
-    momentum = np.append(momentum, pSum) #should be an array of all 4's
+# momentum = []
+# for entry in xrange(0, tree.GetEntries()):
+#     tree.GetEntry(entry)
+#     pSum = 0
+#     for sParticle in sParticles :
+#         pSum += np.linalg.norm(sParticle.getMomentum())
+#     momentum = np.append(momentum, pSum) #should be an array of all 4's
 
-#Generate the plot
+# #Generate the plot
 
-#ROOT
-hist = TH1D('momHist', 'Momentum', 20, -150, 150)
-fill_hist(hist, momentum, None) #takes in weights as the 3rd arg
-c1 = TCanvas("c1")
-hist.SetTitle( "Momentum")
-hist.Draw(" ")
-c2.Print("simParticle_p.pdf")
+# #ROOT
+# hist = TH1D('momHist', 'Momentum', 20, -150, 150)
+# fill_hist(hist, momentum, None) #takes in weights as the 3rd arg
+# c1 = TCanvas("c1")
+# hist.SetTitle( "Momentum")
+# hist.Draw(" ")
+# c2.Print("simParticle_p.pdf")
 
-#Python
-plt.hist(momentum)
-plt.title("3-Momentum Magnitude")
-plt.xlabel("Value")
-plt.ylabel("Frequency")
-plt.show()
+# #Python
+# plt.hist(momentum)
+# plt.title("3-Momentum Magnitude")
+# plt.xlabel("Value")
+# plt.ylabel("Frequency")
+# plt.show()
 
 
 
