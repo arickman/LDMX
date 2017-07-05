8b34efbda65b7ca7be97edc780e279d2766a8a83
arickman <arickman@stanford.edu>
2017-07-05 09:54:07 -0700

    debug

--- simParticle_p.py
+++ simParticle_p.py
@@ -42,6 +42,7 @@ for entry in xrange(0, tree.GetEntries()):
     pSum = 0
     for sParticle in sParticles :
         pSum += np.linalg.norm(sParticle.getMomentum())
+    Print("hello")
     Print(pSum)
     momentum = np.append(momentum, pSum) #should be an array of all 4's
 
