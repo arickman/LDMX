5e8866c3889576fedfb54f182f234e1d2c32b112
arickman <arickman@stanford.edu>
2017-07-11 15:50:16 -0700

    debuggin

--- PNStudies.py
+++ PNStudies.py
@@ -54,7 +54,6 @@ for entry in xrange(0, tree.GetEntries()):
         if sParticle.getPdgID() != 11: continue
         if is_incident(sParticle) :
             incidentElectron = sParticle
-            print("Found it!")
             break
 
     #from the incident e, find the PNGamma that interacted with a nucleus in the target
