3aaff08096d33c4f15c01d7258508eee1e43d261
arickman <arickman@stanford.edu>
2017-07-11 11:04:36 -0700

    print pdg ID

--- simParticle_vertexPosition.py
+++ simParticle_vertexPosition.py
@@ -38,14 +38,19 @@ sParticles = r.TClonesArray('ldmx::SimParticle')
 tree.SetBranchAddress("SimParticles_sim", r.AddressOf(sParticles))
 
 vPos = []
+eCounter = 0
+gCounter = 0
 for entry in xrange(0, tree.GetEntries()):
     tree.GetEntry(entry)
     for sParticle in sParticles :
     	vPos = np.append(vPos, sParticle.getVertex()[2])
     	if -100 < sParticle.getVertex()[2] < 0:
-    		print("Particle Type: " + str(sParticle.getPdgID()))
-    		print("Vertex Position: " + str(sParticle.getVertex()[2]))
-
+    		if sParticle.getPdgID() == 11: eCounter += 1
+    		if sPartcile.getPdgID() == 22: gCounter += 1
+    		#print("Particle Type: " + str(sParticle.getPdgID()))
+    		#print("Vertex Position: " + str(sParticle.getVertex()[2]))
+print("Electron Count: " + str(eCounter))
+print("Gamma Count: " + str(gCounter))
 #Generate the plot
 
 #Python
