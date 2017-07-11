167529e176c604d0dbab896c288341ef491fec97
arickman <arickman@stanford.edu>
2017-07-11 10:56:08 -0700

    print pdg ID

--- simParticle_vertexPosition.py
+++ simParticle_vertexPosition.py
@@ -42,7 +42,9 @@ for entry in xrange(0, tree.GetEntries()):
     tree.GetEntry(entry)
     for sParticle in sParticles :
     	vPos = np.append(vPos, sParticle.getVertex()[2])
-
+    	print("Particle Type: " + str(sParticle.getPdgID()))
+    	print("Vertex Position: " + str(sParticle.getVertex()[2]))
+    	
 #Generate the plot
 
 #Python
