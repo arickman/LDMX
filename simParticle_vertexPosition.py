941f07cc16cd96e36a8d6113caeaf115d4d4eb5a
arickman <arickman@stanford.edu>
2017-07-12 14:19:42 -0700

    overlaid energy plot

--- simParticle_vertexPosition.py
+++ simParticle_vertexPosition.py
@@ -20,6 +20,10 @@ from root_numpy import fill_hist
 from matplotlib.backends.backend_pdf import PdfPages
 from matplotlib.colors import LogNorm
 
+def created_within_target(particle) :
+    if abs(particle.getVertex()[2]) < 0.550 : return True 
+    return False
+
 parser = argparse.ArgumentParser(description='')
 parser.add_argument('-i', action='store', dest='rfile_path', 
                     help='ROOT file to processed.')
@@ -42,15 +46,25 @@ eCounter = 0
 gCounter = 0
 posCounter = 0
 counter = 0
+eVec = []
+posVec = []
+gammaVec = []
 for entry in xrange(0, tree.GetEntries()):
     tree.GetEntry(entry)
     for sParticle in sParticles :
     	vPos = np.append(vPos, sParticle.getVertex()[2])
-    	if -100 < sParticle.getVertex()[2] < 50:
+    	#if -100 < sParticle.getVertex()[2] < 50:
+            if created_within_target(sParticle) :
     		counter += 1
-    		if sParticle.getPdgID() == 11: eCounter += 1
-    		if sParticle.getPdgID() == 22: gCounter += 1
-    		if sParticle.getPdgID() == -11: posCounter += 1
+    		if sParticle.getPdgID() == 11: 
+                            eCounter += 1
+                            eVec = np.append(eVec, sParticle.getEnergy())
+    		if sParticle.getPdgID() == 22: 
+                            gCounter += 1
+                            gammaVec = np.append(gammaVec, sParticle.getEnergy())
+    		if sParticle.getPdgID() == -11: 
+                            posCounter += 1
+                            posVec = np.append(posVec, sParticle.getEnergy())
     		#print("Particle Type: " + str(sParticle.getPdgID()))
     		#print("Vertex Position: " + str(sParticle.getVertex()[2]))
 print("Electron Count: " + str(eCounter))
@@ -67,9 +81,25 @@ plt.ylabel("Frequency")
 plt.show()
 plt.savefig('simParticle_vPos.pdf')
 
+#ROOT
+c1 = TCanvas("c1")
+hist = TH1D('Electron Energy', 'Electron Energy', 20, 0, 5000)
+fill_hist(hist, eVec)
+hist.SetTitle( "Energy of electron produced at Target")
+hist.Draw()
 
+c1 = TCanvas("c1")
+hist2 = TH1D('Positron Energy', 'Positron Energy', 20, 0, 5000)
+fill_hist(hist2, posVec)
+hist2.SetTitle( "Energy of positron produced at Target")
+hist2.Draw()
 
-
+c1 = TCanvas("c1")
+hist3 = TH1D('Gamma Energy', 'Gamma Energy', 20, 0, 5000)
+fill_hist(hist3, gammaVec)
+hist3.SetTitle( "Energy of gamma produced at Target")
+hist3.Draw()
+c1.SaveAs("energiesAtTarget.pdf")
 
 
 
