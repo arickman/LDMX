67ec309f1d6af7891e8bb0b1c67ff1c9b6a02daa
arickman <arickman@stanford.edu>
2017-09-12 10:46:48 -0700

    update queue

--- analysisToBatch.py
+++ analysisToBatch.py
@@ -3,7 +3,7 @@ import os
 def submitJobs(path) :
     fileList = os.listdir(path)
     for i in range(len(fileList)):
-        os.system("bsub /nfs/slac/g/ldmx/users/arickman/LDMX/ENMom_angle.py -i " + fileList[i])
-        os.system("bsub /nfs/slac/g/ldmx/users/arickman/LDMX/ENMom_angle.py -i " + fileList[i])
+        os.system("bsub python /nfs/slac/g/ldmx/users/arickman/LDMX/ENMom_angle.py -i " + fileList[i])
+        os.system("bsub python /nfs/slac/g/ldmx/users/arickman/LDMX/ENMom_angle.py -i " + fileList[i])
 
 submitJobs("/nfs/slac/g/ldmx/production/target_en_output/")
