37f7a981d1f62554e5f376ba06bbd5e96a3b2f88
arickman <arickman@stanford.edu>
2017-09-16 08:45:20 -0700

    path error

--- analysisToBatch.py
+++ analysisToBatch.py
@@ -4,7 +4,26 @@ import os
 def submitJobs(path) :
     fileList = os.listdir(path)
     for i in range(len(fileList)):
-        os.system("bsub python /nfs/slac/g/ldmx/users/arickman/LDMX/ENCounting.py -i " + fileList[i])
-        os.system("bsub python /nfs/slac/g/ldmx/users/arickman/LDMX/ENMom_angle.py -i " + fileList[i])
+        command = ['bash', '-c', 'source /nfs/slac/g/ldmx/users/arickman/LDMX/setup.sh && env']
+        proc = subprocess.Popen(command, stdout=subprocess.PIPE)
+
+        for line in proc.stdout:
+            (key, _, value) = line.partition('=')
+            os.environ[key] = value.strip()
+
+        proc.communicate()
+
+        os.system("bsub python /nfs/slac/g/ldmx/users/arickman/LDMX/ENCounting.py -i /nfs/slac/g/ldmx/production/target_en_output/" + fileList[i])
+
+        command = ['bash', '-c', 'source /nfs/slac/g/ldmx/users/arickman/LDMX/setup.sh && env']
+        proc = subprocess.Popen(command, stdout=subprocess.PIPE)
+
+        for line in proc.stdout:
+            (key, _, value) = line.partition('=')
+            os.environ[key] = value.strip()
+
+        proc.communicate()
+
+        os.system("bsub python /nfs/slac/g/ldmx/users/arickman/LDMX/ENMom_angle.py -i /nfs/slac/g/ldmx/production/target_en_output/" + fileList[i])
 
 submitJobs("/nfs/slac/g/ldmx/production/target_en_output/")
